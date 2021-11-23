a0fa110ac4aa9c95edcfd96c3f1b9a9312cd1c21
OksanaMarysheva <oksana2020marysheva@gmail.com>
2021-11-23 23:23:53 +0200

    Fixed DaysToDate.

--- test-task/src/DaysToDate/DaysToDate.js
+++ test-task/src/DaysToDate/DaysToDate.js
@@ -1,3 +1,14 @@
 export default Date.prototype.daysTo = function(d2) {
-    return Math.floor((d2 - this) / 86400000)
+    let computation = (d2 - this) / 86400000
+    let res
+    if (computation > 0) {
+        res = Math.floor(computation)
+    } else if (computation < 0) {
+        res = Math.abs(Math.ceil(computation))
+    } else if (computation === 0) {
+        res = 0
+    } else {
+        res = 'Something go wrong, please try again'
+    }
+    return res
 }
--- test-task/src/DaysToDate/DaysToDate.test.js
+++ test-task/src/DaysToDate/DaysToDate.test.js
@@ -7,9 +7,16 @@ test('quantity of days between dates', () => {
     expect(difference).toBe(2)
 });
 
-test('quantity of days between dates with hours', () => {
+test('quantity of days d1-d2 between dates with hours', () => {
     const d1 = new Date(2021, 10, 8, 5)
     const d2 = new Date(2021, 10, 10)
+    const difference = d1.daysTo(d2)
+    expect(difference).toBe(1)
+});
+
+test('quantity of days d2-d1 between dates with hours', () => {
+    const d2 = new Date(2021, 10, 8, 5)
+    const d1 = new Date(2021, 10, 10)
     const difference = d1.daysTo(d2)
     expect(difference).toBe(1)
 });
@@ -19,4 +26,11 @@ test('quantity of days between dates with minutes', () => {
     const d2 = new Date(2021, 10, 17)
     const difference = d1.daysTo(d2)
     expect(difference).toBe(2)
+});
+
+test('quantity of days between dates with minutes must be 0', () => {
+    const d1 = new Date(2021, 10, 16, 0, 1)
+    const d2 = new Date(2021, 10, 17)
+    const difference = d1.daysTo(d2)
+    expect(difference).toBe(0)
 });
